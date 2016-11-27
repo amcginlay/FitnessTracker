4d74c98b698e06deabfb213cdbfd17681952f96e
Alan McGinlay <amcginlay@gmail.com>
2016-11-27 17:57:36 +0000

    - Removed commented out code from previous commit - Introduced a resources/messages.properties file which could be used to support internationalisation - wired this concept into the JSP page with spring tags taglib (spring:message code="goal.text") - Servlet config needed new bean definition BUT this time it refused to work with "p.x" param style attributes so reverted to old-style name/value property pair which worked - Included commented-out BeanNameViewResolver bean entry.  It appeared in the demo but was not explained and does not appear necessary.

--- src/main/java/com/cynaptec/controller/MinutesController.java
+++ src/main/java/com/cynaptec/controller/MinutesController.java
@@ -14,16 +14,6 @@ public class MinutesController {
 		System.out.println("exercise: " + exercise.getMinutes());
 		
 		return "addMinutes";
-//		return "forward:addMoreMinutes.html"; // <-- such calls must be .html as we have external context on request
-//		return "redirect:addMoreMinutes.html"; // <-- minutes will get zeroed because redirect is a NEW request
 	}
-
-//	@RequestMapping(value="/addMoreMinutes")
-//	public String addMoreMinutes(@ModelAttribute("exercise") Exercise exercise) {
-//		
-//		System.out.println("exercising: " + exercise.getMinutes());
-//		
-//		return "addMinutes";
-//	}
 
 }
