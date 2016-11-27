a4a1b712afe78ce6b5e1b9b496ca96cebdcbaf8c
Alan McGinlay <amcginlay@gmail.com>
2016-11-27 15:59:20 +0000

    - Updated web.xml to declare servlet and servlet-mapping - Added WEB-INF/config/servlet-config.xml (this is a Spring Bean Configuration File with "context", "mvc" and "p" namespaces enabled) - Add the following to the new servlet-config.xml  file:  	<mvc:annotation-driven/> 	<context:component-scan base-package="com.cynaptec.controller"></context:component-scan> 	<bean class="org.springframework.web.servlet.view.InternalResourceViewResolver" p:prefix="/WEB-INF/jsp/" p:suffix=".jsp"/> - Created a controller class to unite the greeting URL with the hello.jsp page (see later), adding a keyed message to the attributes which the JSP will bind to - Created a basic JSP page adding the following between the body tags: 	<h1>${greeting}</h1>
    .springBeans and .project files were created so adding these too.

---
+++ src/main/java/com/cynaptec/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.cynaptec.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	@RequestMapping(value="/greeting")
+	public String sayHello(Model model) {
+		model.addAttribute("greeting", "Hello, Spring Web MVC");
+		return "hello";
+	}
+}
